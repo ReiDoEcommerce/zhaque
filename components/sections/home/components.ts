bec7f551d9d246565813f06e2d678db4b7264512
Tiago Dias Maciel <tiago.maciel@leanwork.com.br>
2022-10-21 21:35:15 -0300

    feat: fix bus components

--- components/sections/home/components.ts
+++ components/sections/home/components.ts
@@ -1,10 +1,8 @@
 import { BannerComponent } from "./banner";
-import { CollectionComponent } from "./collection-list";
 import { PartnersComponent } from "./partners"
 
 export {
   BannerComponent,
-  CollectionComponent,
   PartnersComponent,
 };
 
